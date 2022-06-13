5f337991d270d370f04c03104c1ca4119ed30b6d
koushik <koushik.dey@live.com>
2022-06-13 18:15:47 +0300

    sdR

--- pr1.py
+++ pr1.py
@@ -5,3 +5,19 @@ class Employee:
 
 emp_1 = Employee()
 emp_2 = Employee()
+
+print(emp_1)
+print(emp_2)
+
+emp_1.first_name = 'Koushik'
+emp_1.last_name = 'Dey'
+emp_1.email = 'koushik.dey@company.com'
+emp_1.salary = 50000
+
+emp_2.first_name = 'Rajon'
+emp_2.last_name = 'Dey'
+emp_2.email = 'rajon.dey@company.com'
+emp_2.salary = 60000
+
+print(emp_1.email)
+print(emp_2.email)
